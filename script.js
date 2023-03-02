e79d9635011557bffa48b9cd0800d28df2fdcdb8
timothybaraka <timothybaraka44@gmail.com>
2023-03-02 11:28:10 +0300

    Add javascript to increase functionality

--- script.js
+++ script.js
@@ -2,21 +2,35 @@ window.addEventListener("load", ()=>{
     const input = document.getElementById("upload");
         const filewrapper =document.getElementById("filewrapper")
 
-    
     input.addEventListener("change", (e)=>{
-        let fileName
-    = e.target.files[0].name;
+        let fileName = e.target.files[0].name;
     let filetype= e.target.value.split(".").pop(); 
     fileshow(fileName, filetype);
-
-    })
+})
+    
     const fileshow=(fileName, filetype)=>{
-    const showfileboxElem= document.createElement("div");
+    const showfileboxElem= document.createElement("div"); 
     showfileboxElem.classList.add("showfilebox");
-    const leftElem = document.createElement("div"); 
-    const fileTypeElement = dosumentcreateElement('spam');
-    leftElem.classList.add("left"); I
+    const leftElem = document.createElement("div");
+    leftElem.classList.add("left");
+    const fileTypeElem = document.createElement("span");
+    fileTypeElem.classList.add("filetype");
+    fileTypeElem.innerHTML=filetype;
+    leftElem.append(fileTypeElem);
+    const filetitleElem = document.createElement("h3");
+    filetitleElem.innerHTML=fileName;
+    leftElem.append(filetitleElem);
     showfileboxElem.append(leftElem);
+    const rightElem = document.createElement("div");
+    rightElem.classList.add("right"); I
+    showfileboxElem.append(rightElem);
     filewrapper.append(showfileboxElem);
+    crossElem.innerHTML="&#215;";
+    rightElem.append(crossElem);
+    filewrapper.append(showfileboxElem);    
+   
+    crossElem.addEventListener("click", ()=> {
+        filewrapper.removeChild(showfileboxElem);
+    })
     }
 })
